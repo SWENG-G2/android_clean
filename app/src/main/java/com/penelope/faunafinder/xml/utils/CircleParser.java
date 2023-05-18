040191a8384476d67ce6565d09c8adac6b214e5a
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-05-18 18:53:18 +0100

    Implement CircleParser

--- app/src/main/java/com/penelope/faunafinder/xml/utils/CircleParser.java
+++ app/src/main/java/com/penelope/faunafinder/xml/utils/CircleParser.java
@@ -1,4 +1,27 @@
 package com.penelope.faunafinder.xml.utils;
 
-public class CircleParser {
+import com.penelope.faunafinder.presentation.elements.CircleElement;
+
+import org.xmlpull.v1.XmlPullParser;
+
+/**
+ * <code>CircleParser</code> is a utility class used to parse circle elements from presentation.
+ */
+public class CircleParser extends ElementParser {
+    /**
+     * Parses a circle element from an {@link XmlPullParser} at the right position.
+     *
+     * @param xmlPullParser The XmlPullParser.
+     * @return A set-up {@link CircleElement} with fallback default values.
+     */
+    public static CircleElement parseCircle(XmlPullParser xmlPullParser) {
+        int radius = parseInt(xmlPullParser.getAttributeValue(NAME_SPACE, RADIUS));
+        int colour = parseColour(xmlPullParser.getAttributeValue(NAME_SPACE, COLOUR));
+        int borderWidth = parseInt(xmlPullParser.getAttributeValue(NAME_SPACE, BORDER_WIDTH));
+        int borderColour = parseColour(xmlPullParser.getAttributeValue(NAME_SPACE, BORDER_COLOUR));
+        int x = parseInt(xmlPullParser.getAttributeValue(NAME_SPACE, X_COORDINATE));
+        int y = parseInt(xmlPullParser.getAttributeValue(NAME_SPACE, Y_COORDINATE));
+
+        return new CircleElement(radius, colour, borderWidth, borderColour, x, y);
+    }
 }
